6d3086296ef71ffb5d8eda976c27f4ff2ce9d36c
Denis <deniskulygin@gmail.com>
2020-05-11 22:32:32 +0300

    Binary Studio Academy PHP 2020 PHP7 Task 3

--- src/Task3/CarTrackHtmlPresenter.php
+++ src/Task3/CarTrackHtmlPresenter.php
@@ -4,13 +4,32 @@ declare(strict_types=1);
 
 namespace App\Task3;
 
-use App\Task1\Car;
 use App\Task1\Track;
 
+/**
+ * Class CarTrackHtmlPresenter
+ *
+ * @package App\Task3
+ */
 class CarTrackHtmlPresenter
 {
+    /**
+     * @var string
+     */
+    private $output;
+    
+    /**
+     * @param Track $track
+     *
+     * @return string
+     */
     public function present(Track $track): string
     {
-        //@todo
+        foreach ($track->all() as $car) {
+            $this->output .= '<li>' . '<h2>' . $car->getName() . '</h2>' . '</li>';
+            $this->output .= '<li>' . '<img src="' . $car->getImage() . '" alt="'. $car->getName() . '">' .'</li>';
+        }
+        
+        return '<ul class="timeline">' . $this->output . '</ul>';
     }
 }
