16712c45ae7dc094fb491d00f6151dfdcda76374
Denis <deniskulygin@gmail.com>
2020-05-11 22:30:34 +0300

    Binary Studio Academy PHP 2020 PHP7 Task 2

--- src/Task2/Book.php
+++ src/Task2/Book.php
@@ -4,20 +4,63 @@ declare(strict_types=1);
 
 namespace App\Task2;
 
-class Book
+/**
+ * Class Book
+ *
+ * @package App\Task2
+ */
+class Book implements BookInterface
 {
+    /**
+     * @var string
+     */
+    private $title;
+    
+    /**
+     * @var int
+     */
+    private $price;
+    
+    /**
+     * @var int
+     */
+    private $pagesNumber;
+    
+    /**
+     * Book constructor.
+     *
+     * @param string $title
+     * @param int    $price
+     * @param int    $pagesNumber
+     */
+    public function __construct(string $title, int $price, int $pagesNumber)
+    {
+        $this->title = $title;
+        $this->price = $price;
+        $this->pagesNumber = $pagesNumber;
+    }
+    
+    /**
+     * @return string
+     */
     public function getTitle(): string
     {
-        // @todo
+        return $this->title;
     }
-
+    
+    /**
+     * @return int
+     */
     public function getPrice(): int
     {
-        // @todo
+        return $this->price;
     }
-
+    
+    /**
+     * @return int
+     */
     public function getPagesNumber(): int
     {
-        //@todo
+        return $this->pagesNumber;
     }
 }
